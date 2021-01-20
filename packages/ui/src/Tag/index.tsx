1cf35657d18e9a20d3a4330ad22922e68722e9ef
戴曦 <daix@mingyuanyun.com>
2021-01-20 11:27:15 +0800

    修改

--- packages/ui/src/Tag/index.tsx
+++ packages/ui/src/Tag/index.tsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import classnames from 'classnames';
 import classes from './index.less';
 
-const Tag = () => {
-  return <div className={classes.app}>Tag</div>;
+type Props = {
+  type?: 'default' | 'blue' | 'red' | 'top10' | 'quality';
+};
+
+const Tag: React.FC<React.HTMLProps<HTMLSpanElement> & Props> = ({
+  type = 'default',
+  ...props
+}) => {
+  return (
+    <span
+      {...props}
+      className={classnames(
+        classes.tag,
+        classes['tag-' + type],
+        props.className
+      )}
+    />
+  );
 };
 
 export default Tag;
